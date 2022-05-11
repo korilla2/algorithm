1fbf940fc48ad155fdc2e4ef59422a7315bf16b1
korilla2 <kingdoss@naver.com>
2022-05-11 11:29:13 +0900

    연습

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-print('이름을 입력하세요.: ', end='')
-name = input()
-print(name)
+cnt = 0
+
+temp = dict()
+
+temp[0] = 2
+temp[1] = 3
+
+
+ptr = 2
+
+for n in range(5, 1001, 2):
+    i = 1
+    while temp[i] ** 2 <= n:
+        cnt += 2
+        if n % temp[i] == 0:
+            break
+        i += 1
+
+    else:
+        temp[ptr] = n
+        ptr += 1
+        cnt += 1
+print(cnt)
+print(temp)
